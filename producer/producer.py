40cd2f0f10c6cae221bd44a34bf63aab88e50afa
evelinn11 <evelinalim20@gmail.com>
2026-05-28 22:08:57 +0700

    fix: Correcting the external port and topic name
    Port externalnya salah dan nama topicnya ga konsisten

--- producer/producer.py
+++ producer/producer.py
@@ -4,7 +4,7 @@ import json
 import time
 
 producer = KafkaProducer(
-    bootstrap_servers='localhost:9092',
+    bootstrap_servers='localhost:29092',
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
@@ -32,7 +32,7 @@ for _, row in df.iterrows():
     }
 
     producer.send(
-        'clickstream-fashion-events',
+        'clickstream-events',
         value=event
     )
 
